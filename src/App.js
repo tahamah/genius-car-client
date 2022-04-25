3e45ed29600a900f24226a9ce1b19f7062b27c34
unknown <taha.iu.bd@gmail.com>
2022-04-25 14:22:03 +0600

    before-jwt

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import './App.css'
 import About from './Pages/About/About'
 import AddService from './Pages/AddService/AddService'
@@ -8,6 +9,7 @@ import Login from './Pages/Login/Login/Login'
 import Register from './Pages/Login/Register/Register'
 import RequireAuth from './Pages/Login/RequireAuth/RequireAuth'
 import ManageServices from './Pages/ManageServices/ManageServices'
+import Order from './Pages/Order/Order'
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail'
 import Footer from './Pages/Shared/Footer/Footer'
 import Header from './Pages/Shared/Header/Header'
@@ -28,7 +30,7 @@ function App() {
                 <Route path="/login" element={<Login></Login>}></Route>
                 <Route path="/register" element={<Register></Register>}></Route>
                 <Route
-                    path="/checkout"
+                    path="/checkout/:serviceId"
                     element={
                         <RequireAuth>
                             <Checkout></Checkout>
@@ -51,9 +53,18 @@ function App() {
                         </RequireAuth>
                     }
                 />
+                <Route
+                    path="/orders"
+                    element={
+                        <RequireAuth>
+                            <Order />
+                        </RequireAuth>
+                    }
+                />
                 <Route path="*" element={<NotFound></NotFound>}></Route>
             </Routes>
             <Footer></Footer>
+            <ToastContainer />
         </div>
     )
 }
--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,11 +1,99 @@
-import React from 'react';
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import useServiceDetail from '../../../hooks/useServiceDetail'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import auth from '../../../firebase.init'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Checkout = () => {
-    return (
-        <div>
-            <h2>Please Checkout your booking</h2>
-        </div>
-    );
-};
+    const { serviceId } = useParams()
+    const [service] = useServiceDetail(serviceId)
+    const [user] = useAuthState(auth)
 
-export default Checkout;
+    const handelPlaceOrder = (e) => {
+        e.preventDefault()
+        const order = {
+            email: e.target.email.value,
+            service: service.name,
+            serviceId: serviceId,
+            address: e.target.address.value,
+            phone: e.target.phone.value,
+        }
+        axios
+            .post('http://localhost:5000/order', order)
+            .then(function (response) {
+                const { data } = response
+                if (data?.insertedId) {
+                    toast('Your order is booked')
+                    e.target.reset()
+                }
+            })
+    }
+    return (
+        <div className="w-50 mx-auto mt-5">
+            <h2 className="text-primary text-center">
+                Please Order: {service.name}
+            </h2>
+            <form onSubmit={handelPlaceOrder}>
+                <input
+                    className="w-100 mb-2"
+                    type="text"
+                    name="name"
+                    value={user?.displayName}
+                    placeholder="Your Name"
+                    id=""
+                    readOnly
+                    disabled
+                />
+                <br />
+                <input
+                    className="w-100 mb-2"
+                    type="email"
+                    name="email"
+                    value={user?.email}
+                    placeholder="Your Email"
+                    id=""
+                    readOnly
+                    disabled
+                />
+                <br />
+                <input
+                    className="w-100  mb-2"
+                    type="text"
+                    name="service"
+                    value={service.name}
+                    placeholder="Service"
+                    id=""
+                    readOnly
+                    disabled
+                />
+                <br />
+                <input
+                    className="w-100 mb-2"
+                    type="text"
+                    name="address"
+                    placeholder="Your Address"
+                    id=""
+                    autoComplete="off"
+                />
+                <br />
+                <input
+                    className="w-100 mb-2"
+                    type="number"
+                    name="phone"
+                    placeholder="Your Phone Number"
+                    id=""
+                />
+                <br />
+                <input
+                    className="btn btn-primary"
+                    type="submit"
+                    value="Place Order"
+                />
+            </form>
+        </div>
+    )
+}
+
+export default Checkout
